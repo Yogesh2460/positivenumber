fc2b39c6b2b8bfc687df40c1d6fd0a00731844f9
YOGESH L <92539127+Yogesh2460@users.noreply.github.com>
2022-08-24 23:46:57 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,10 @@
+list1=[]
+n=int(input("Enter size of list "))
+for i in range(0,n):
+    e=int(input("Enter element of list "))
+    list1.append(e)
+
+for i in list1:   
+
+    if i >= 0:
+       print(i, end = " ")
